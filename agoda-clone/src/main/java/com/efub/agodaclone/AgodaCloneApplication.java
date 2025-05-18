9bca61be83c13f1aa1061f8662efdbe262fdc801
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-05-19 02:21:48 +0900

    Chore: init setting #1

---
+++ agoda-clone/src/main/java/com/efub/agodaclone/AgodaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.efub.agodaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgodaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgodaCloneApplication.class, args);
+    }
+
+}
