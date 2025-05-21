5d31ef5d63200fea94efc7a3082f49dde7216806
s2eojeong <155219307+s2eojeong@users.noreply.github.com>
2025-05-21 21:24:00 +0900

    feat: 예약하기 & 나의 예약 조회 API 구현

---
+++ agoda-clone/src/main/java/com/efub/agodaclone/reservation/controller/ReservationController.java
@@ -0,0 +1,35 @@
+package com.efub.agodaclone.reservation.controller;
+
+import com.efub.agodaclone.reservation.dto.ReservationConfirmationResponseDto;
+import com.efub.agodaclone.reservation.dto.ReservationListResponseDto;
+import com.efub.agodaclone.reservation.dto.ReservationRequestDto;
+import com.efub.agodaclone.reservation.service.ReservationService;
+import jakarta.validation.Valid;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+//    // 예약하기
+//    @PostMapping
+//    public ResponseEntity<ReservationConfirmationResponseDto> makeReservation(@RequestBody @Valid ReservationRequestDto requestDto){
+//        ReservationConfirmationResponseDto responseDto = reservationService.addReservation(requestDto);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+//    }
+//
+//    // 다가오는 예약 조회
+//    @GetMapping("/upcoming")
+//    public ResponseEntity<ReservationListResponseDto> searchUpcomingReservations(){
+//        ReservationListResponseDto responseDto = reservationService.searchUpcomingReservations();
+//        return ResponseEntity.ok(responseDto);
+//    }
+}
